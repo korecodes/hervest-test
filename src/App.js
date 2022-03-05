e5a82b9e726e581d7799f9d8764a8e42eb48373f
Daniel Apantaku <apantakuayo@gmail.com>
2022-03-05 19:25:00 +0100

    setup with tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-5xl">FINESSE</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
